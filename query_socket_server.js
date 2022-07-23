6abe5ac94e2606ae21f7c7d1bf55d3ef410f6fd7
salvoch <github@salvoch.com>
2022-07-23 11:13:56 -0600

    Working mem setblock from server

--- query_socket_server.js
+++ query_socket_server.js
@@ -5,17 +5,22 @@ This code will query that server, and print the return text
 var client = new Socket();
 
 client.connect(8064, 'localhost', function() {
-    var response = '';
+    var response;
 
     client.on('data', function(data) {
-        response += data.toString();
+        //response += data.toString();
+        //response += data;
+        response = data;
     });
 
     client.on('end', function() {
-        console.log(typeof(response));
-        console.log(response);
+        //console.log(typeof(response));
+        //console.log(response);
+        console.log(response.length);
+        mem.setblock(0x80162F20, response);
 
-        //buffer
+        /*
+        //Convert string to "object"
         var myBuffer = [];
         var buffer = new Buffer(response, 'utf16le');
         for (var i = 0; i < buffer.length; i++) {
@@ -23,6 +28,7 @@ client.connect(8064, 'localhost', function() {
         }
         console.log(typeof(myBuffer));
         console.log(myBuffer);
+        */
     });
 
     const message = [
