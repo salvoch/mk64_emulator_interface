be33d8318a47e6d6373bffd7b7d7f4f370fb30ae
salvoch <github@salvoch.com>
2022-07-27 00:31:14 -0600

    Create onexec function for character compare just a test funciton, we can set GPR values here! Nice...

---
+++ gpr.js
@@ -0,0 +1,7 @@
+console.log("GPR T2: ")
+console.log(cpu.gpr.t2);
+
+
+console.log("SET T2: ")
+cpu.gpr.t2 = 5;
+console.log(cpu.gpr.t2);
--- mk64.js
+++ mk64.js
@@ -4,6 +4,35 @@ console.log("Running MK64 script...");
 
 //-----Events-----
 //Event to trigger when game checks if stage has saved ghost, trick game into thinking it does!
+events.onexec(0x80091F60, function(e) {
+    debug.breakhere(true);
+
+    //TODO -- query server here? If ghost exists then continue with the t2 manipulation, and the rest of the functions
+    //Thoughts -- should we register the other read-callbacks here? And de-register them at the end? That way we only proceed
+    //            if we are actually loading a ghost!?
+    console.log("Character compare command triggered!");
+
+    //logic here --- queries server
+    //if server says yes:
+        // trick game into thinking laoding
+        // register callback executes here?? OR Set a global flag to load
+
+    console.log("GPR T2: ")
+    console.log(cpu.gpr.t2);
+    console.log("GPR S2: ")
+    console.log(cpu.gpr.s2);
+
+    console.log("Set to each other")
+    cpu.gpr.t2 = cpu.gpr.s2
+
+    console.log("GPR T2: ")
+    console.log(cpu.gpr.t2);
+    console.log("GPR S2: ")
+    console.log(cpu.gpr.s2);
+
+    debug.resume();
+
+});
 
 //Event when ghost charg ID is read for rendering. Chars IDs are 0-8
 events.onread(0x80162F20, function(e) {
@@ -12,6 +41,7 @@ events.onread(0x80162F20, function(e) {
     var json_payload = {"Function": "ghost_character_load", "TrackID": 0};
     query_server(json_payload, write_ghost_char);
 });
+
 
 //Event when MIO0 drive data is read to be decompressed
 
