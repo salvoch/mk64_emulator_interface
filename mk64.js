e92ae0d76d7bb4598a31be41f1e7ec04bc7e8483
salvoch <github@salvoch.com>
2022-07-27 14:26:49 -0600

    Write notes on new structure, create global variables notes on how the program will be structured, we will only have to query the server once with this, communicate via json
    Also, create global variables that this new plan will use

--- mk64.js
+++ mk64.js
@@ -1,7 +1,11 @@
 /*MK64 is the script which interfaces with the Mario Kart 64 (EU) ROM using the latest
 PJ64 emulator. It also interfaces with the "MK64 Watcher.exe" time and ghost manager*/
+var WRITE_FLAG = false;
+var CHARACTER_ID = null;
+var TRACK_ID = null;
+var MIO0_DATA = null;
+
 console.log("Running MK64 script...");
-var writeFlag = false;
 
 //-----Events-----
 //Event to trigger when game checks if stage has saved ghost, trick game into thinking it does!
@@ -9,7 +13,9 @@ events.onexec(0x80091F60, function(e) {
     debug.breakhere(true);
     console.log("Character compare command triggered!");
     var json_payload = {"Function": "check_for_ghost", "TrackID": 0};
-    query_server(json_payload, set_write_flag);
+    var somevar = query_server(json_payload, set_write_flag);
+    console.log("here");
+    set_write_flag(somevar);
 });
 
 //Event when ghost char ID is read for rendering. Chars IDs are 0-8
@@ -17,7 +23,7 @@ events.onread(0x80162F20, function(e) {
     debug.breakhere(true);
 
     //Check to make sure we actually want to write a ghost
-    if (writeFlag) {
+    if (WRITE_FLAG) {
         console.log("Character read triggered!");
         var json_payload = {"Function": "ghost_character_load", "TrackID": 0};
         query_server(json_payload, write_ghost_char);
@@ -31,7 +37,7 @@ events.onexec(0x8000520C, function(e) {
     debug.breakhere(true);
 
     //Check to make sure we actually want to write a ghost
-    if (writeFlag) {
+    if (WRITE_FLAG) {
         console.log("MIO0 read triggered!");
         var json_payload = {"Function": "MIO0_load", "TrackID": 0};
         query_server(json_payload, read_MIO0_file);
@@ -40,13 +46,13 @@ events.onexec(0x8000520C, function(e) {
     }
 });
 
-
 //Event when ghost is attempted to be saved
 
 //-----Callbaks-----
 //Function to write ghost, then resume emulator
 function write_ghost_char(data){
     //parse data response here
+    console.log("Hello2");
     mem.setblock(0x80162F20, data);
     debug.resume();
 }
@@ -55,7 +61,7 @@ function write_ghost_char(data){
 function set_write_flag(data){
     //parse data response here
     //IF response === true or something, then:
-    writeFlag = true;
+    WRITE_FLAG = true;
 
     //Call the function that tricks the game here??
     course_match();
@@ -70,12 +76,13 @@ function read_MIO0_file(data){
     //Open the file that has MIO0 data bytes
     //process file into bytes
     var MIO0_byte_stream;
+    var jsonfile = fs.open('tempfiles/ghost-dump.json', )
 
     //Call function to actually inject ghost, send MIO0 data bytes
     inject_MIO0(MIO0_byte_stream)
 
     //Reset flag, since we are done with the injection process
-    writeFlag = false;
+    WRITE_FLAG = false;
 }
 
 //-----Functions-----
@@ -93,6 +100,8 @@ function query_server(payload, fn){
     
         client.on('end', function() {
             fn(response);
+
+            return(response);
         });
     
         const message = [
@@ -114,3 +123,26 @@ function inject_MIO0(MIO0_stream){
 function course_match(){
     cpu.gpr.t2 = cpu.gpr.s2;
 }
+
+
+/*
+execbreak (compare is done)
+    reach out to server with --> (bool TrackID, string "read_ghost")
+    Server responds: {"available": <bool>, "path": <string>}
+        available?: bool ->
+            false (do nothing) *BREAK*
+            true (server has written the intermediate file)
+                -> trick the game
+                -> parse json, store varialbes globally
+                -> continue emulator
+                -> *game runs checksum*
+                -> break
+                    -> inject character ID (on read)
+                -> break
+                    -> Inject MIO0 (on read)
+                -> reset flags/globals
+                    -> Runs again on restart? Check?
+
+    WRITE GHOST
+        ...
+*/
