00d62f5cf880b80b3fdf9f9056a97e9f5c9229a6
salvoch <github@salvoch.com>
2022-07-26 23:26:26 -0600

    Add place holders for some functions

--- mk64.js
+++ mk64.js
@@ -3,13 +3,19 @@ PJ64 emulator. It also interfaces with the "MK64 Watcher.exe" time and ghost man
 console.log("Running MK64 script...");
 
 //-----Events-----
-//When ghost cha ID is read for rendering. Chars IDs are 0-8
+//Event to trigger when game checks if stage has saved ghost, trick game into thinking it does!
+
+//Event when ghost charg ID is read for rendering. Chars IDs are 0-8
 events.onread(0x80162F20, function(e) {
     debug.breakhere(true);
-    console.log("Char read triggered!");
+    console.log("Character read triggered!");
     var json_payload = {"Function": "ghost_character_load", "TrackID": 0};
     query_server(json_payload, write_ghost_char);
 });
+
+//Event when MIO0 drive data is read to be decompressed
+
+//Event when ghost is attempted to be saved
 
 //-----Callbaks-----
 //Function to write ghost, then resume emulator
@@ -22,6 +28,7 @@ function write_ghost_char(data){
 //-----Functions-----
 
 //query the C# server, arg JSON payload, and callback function
+//Calls fn with response
 function query_server(payload, fn){
     var client = new Socket();  
     client.connect(8064, 'localhost', function() {
@@ -43,3 +50,4 @@ function query_server(payload, fn){
     });
 }
 
+//Function to read MIO0 file
