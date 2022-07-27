f7620be92ee796bf1e0ce22499ac019f6e105b0e
salvoch <github@salvoch.com>
2022-07-27 11:21:41 -0600

    Utilize writeFlag writeFlag has been utilized to trigger the onread events, check those first

--- mk64.js
+++ mk64.js
@@ -1,48 +1,27 @@
 /*MK64 is the script which interfaces with the Mario Kart 64 (EU) ROM using the latest
 PJ64 emulator. It also interfaces with the "MK64 Watcher.exe" time and ghost manager*/
 console.log("Running MK64 script...");
-var writeFlag = false;
+var writeFlag = false; //TODO - Don't forget to set this back to false at some point! (After MIO0 load?)
 
 //-----Events-----
 //Event to trigger when game checks if stage has saved ghost, trick game into thinking it does!
 events.onexec(0x80091F60, function(e) {
     debug.breakhere(true);
-
-    //Query server
-
-    //TODO -- query server here? If ghost exists then continue with the t2 manipulation, and the rest of the functions
-    //Thoughts -- should we register the other read-callbacks here? And de-register them at the end? That way we only proceed
-    //            if we are actually loading a ghost!?
     console.log("Character compare command triggered!");
-
-    //logic here --- queries server
-    //if server says yes:
-        // trick game into thinking laoding
-        // register callback executes here?? OR Set a global flag to load
-
-    console.log("GPR T2: ")
-    console.log(cpu.gpr.t2);
-    console.log("GPR S2: ")
-    console.log(cpu.gpr.s2);
-
-    console.log("Set to each other")
-    cpu.gpr.t2 = cpu.gpr.s2
-
-    console.log("GPR T2: ")
-    console.log(cpu.gpr.t2);
-    console.log("GPR S2: ")
-    console.log(cpu.gpr.s2);
-
-    debug.resume();
-
+    var json_payload = {"Function": "check_for_ghost", "TrackID": 0};
+    query_server(json_payload, set_write_flag);
 });
 
-//Event when ghost charg ID is read for rendering. Chars IDs are 0-8
+//Event when ghost char ID is read for rendering. Chars IDs are 0-8
 events.onread(0x80162F20, function(e) {
     debug.breakhere(true);
-    console.log("Character read triggered!");
-    var json_payload = {"Function": "ghost_character_load", "TrackID": 0};
-    query_server(json_payload, write_ghost_char);
+    if (writeFlag) {
+        console.log("Character read triggered!");
+        var json_payload = {"Function": "ghost_character_load", "TrackID": 0};
+        query_server(json_payload, write_ghost_char);
+    } else {
+        debug.resume();
+    }
 });
 
 
@@ -55,6 +34,19 @@ events.onread(0x80162F20, function(e) {
 function write_ghost_char(data){
     //parse data response here
     mem.setblock(0x80162F20, data);
+    debug.resume();
+}
+
+//Function that determines if we are to inject ghost data or not!
+function set_write_flag(data){
+    //parse data response here
+    //IF response === true or something, then:
+    writeFlag = true;
+
+    //Call the function that tricks the game here??
+    course_match();
+
+    //Resume game
     debug.resume();
 }
 
@@ -84,3 +76,8 @@ function query_server(payload, fn){
 }
 
 //Function to read MIO0 file
+
+//Function that tricks game into thinking the header course save file matches the selected course
+function course_match(){
+    cpu.gpr.t2 = cpu.gpr.s2;
+}
